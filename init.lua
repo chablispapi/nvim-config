6b03a3d402c3d6bba1deeb2378cb037899d363b1
Gennari Lorenzo <lrnzgnnr@gmail.com>
2024-11-27 23:57:36 +0100

    find recent files with <leader>.

--- init.lua
+++ init.lua
@@ -88,6 +88,8 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "FF", ":vertical resize +5<CR>", { desc = "Resize window +5" })
+vim.keymap.set("n", "ff", ":vertical resize -5<CR>", { desc = "Resize window -5" })
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -16,16 +16,17 @@ return { -- You can easily change to a different colorscheme.
 		vim.cmd.hi("Comment gui=none")
 	end,
 }
+--]]
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
 	init = function()
-		vim.cmd.colorscheme("catppuccin-frappe")
+		vim.cmd.colorscheme("catppuccin-latte")
 		vim.cmd.hi("Comment gui=none")
 	end,
 }
---]]
+--[[
 return {
 	"sho-87/kanagawa-paper.nvim",
 	lazy = false,
@@ -36,3 +37,4 @@ return {
 	end,
 	opts = {},
 }
+--]]
